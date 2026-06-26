4944a800afbc2eaa54e0a8db2a84effa80519c74
pritk <pritk@ebay.com>
2026-06-26 10:25:48 +0530

    Add Sample Route

--- src/main/java/com/tess/HelloWorld.java
+++ src/main/java/com/tess/HelloWorld.java
@@ -1,7 +1,26 @@
 package com.tess;
 
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+
+        server.createContext("/hello", exchange -> {
+            byte[] body = "{\"message\": \"Hello, World!\"}".getBytes();
+            exchange.getResponseHeaders().set("Content-Type", "application/json");
+            exchange.sendResponseHeaders(200, body.length);
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(body);
+            }
+        });
+
+        server.start();
+        System.out.println("Server running on http://localhost:" + port);
+        System.out.println("Route: GET /hello");
     }
 }
